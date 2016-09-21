8c04f9797c263ffb5a787ab2461c8658bbd3b0b4
Shelin Anton <shelinanton@gmail.com>
2016-09-21 12:09:54 +0300

    fixed sigmoid and log tables init

--- FastText/Utils.fs
+++ FastText/Utils.fs
@@ -10,19 +10,20 @@ module Utils =
     let LOG_TABLE_SIZEf = float(LOG_TABLE_SIZE)
 
     let t_sigmoid = Array.zeroCreate (SIGMOID_TABLE_SIZE + 1)
-    let t_log = Array.zeroCreate (LOG_TABLE_SIZE + 1)
-
     
-    for i in 0..(SIGMOID_TABLE_SIZE + 1) do
+    for i in 0..SIGMOID_TABLE_SIZE do
         let x = float(i * 2 * MAX_SIGMOID) / SIGMOID_TABLE_SIZEf - MAX_SIGMOIDf
         t_sigmoid.[i] <- 1.0 / (1.0 + exp(-x))
-    for i in 0..(LOG_TABLE_SIZE + 1) do
+
+    let t_log = Array.zeroCreate (LOG_TABLE_SIZE + 1)
+    for i in 0..LOG_TABLE_SIZE do
         let x = (float(i) + 1e-5) / LOG_TABLE_SIZEf
         t_log.[i] <- log(x)
 
     let log(x : float) =
         if x > 1.0 then 0.0
-        else t_log.[int(x) * LOG_TABLE_SIZE]
+        else let i = int(x * LOG_TABLE_SIZEf)
+             t_log.[i]
 
     let sigmoid(x : float) =
         if x < float(-MAX_SIGMOID) then 0.0
